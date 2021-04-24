f8d7d24095ace2f9fa97feab6b5c67bb11ffbf42
mmkvdev <madhuv299@gmail.com>
2021-04-24 12:16:37 +0530

    func/ts_objects: typescript objects

---
+++ project/objects/objects.ts
@@ -0,0 +1,19 @@
+// objects
+
+let _developer = {
+    _name: 'cloud aws',
+    _age: 23,
+    _role: 'software'
+};
+
+_developer._age = 24;
+_developer._name= 'multi-cloud';
+// developer._age = '23';
+// developer._skills = ['coding', 'database'];
+
+_developer = {
+    _name: 'backend developer',
+    _age: 24,
+    _role: 'api'
+    // skills: [] 
+};
