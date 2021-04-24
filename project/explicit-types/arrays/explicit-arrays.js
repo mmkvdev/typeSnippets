afcc541821daa35a90ff2eef30b50c7b7528131d
mmkvdev <madhuv299@gmail.com>
2021-04-24 12:14:12 +0530

    func/ts-explicit: explicit typed arrays and general variables

---
+++ project/explicit-types/arrays/explicit-arrays.js
@@ -0,0 +1,5 @@
+// explicit array types
+var developer_stack = [];
+// developer_stack = [2,3]
+developer_stack = ['aws', 'python'];
+developer_stack.push('ionic', 'angular');
---
+++ project/explicit-types/varaibles/explicit-variables.ts
@@ -0,0 +1,14 @@
+// explicit types
+
+let developer_: string;
+let age_: number;
+let skills_: Array<Object>;
+
+// age = 'aws';
+age_ = 24;
+skills_ = [
+    { 
+        name: 'aws', techstack: ['python', 'aws']
+    },
+    { stack: 'python' }
+];
