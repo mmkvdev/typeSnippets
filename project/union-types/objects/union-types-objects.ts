03c8a78d176912a2f1d88fd3bc651d5acca30a88
mmkvdev <madhuv299@gmail.com>
2021-04-24 12:18:53 +0530

    func/union_types_arrays: typescript union typed objects and variables

---
+++ project/union-types/objects/union-types-objects.ts
@@ -0,0 +1,14 @@
+// objects
+
+let _developerLevel: object;
+
+_developerLevel = { name: 'aws architect', level: 'architect' }; //valid
+
+
+// structuring or defining the property types inside the object
+let _developerLevelBeta: {
+    name: string,
+    level: string,
+    skilledScore: number,
+    acceptanceLevel: string,
+};
---
+++ project/union-types/variables/union-types-variables.js
@@ -0,0 +1,5 @@
+// union types on variables
+var uid;
+uid = 'aws_developer';
+uid = 23;
+// uid = true;
