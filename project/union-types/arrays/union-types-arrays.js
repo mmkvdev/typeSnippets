81aad8c0da356614994e9187627a7ffa7ccd5d88
mmkvdev <madhuv299@gmail.com>
2021-04-24 12:17:50 +0530

    func/union_types_arrays: typescript union typed arrays

---
+++ project/union-types/arrays/union-types-arrays.js
@@ -0,0 +1,6 @@
+// union-types
+var union_types = [];
+union_types.push('aws');
+union_types.push(23);
+union_types.push(true);
+console.log('union_types: ', union_types);
---
+++ project/union-types/arrays/union-types-arrays.ts
@@ -0,0 +1,9 @@
+// union-types
+
+let _union_types: (string|number|boolean)[] = [];
+
+_union_types.push('aws');
+_union_types.push(23);
+_union_types.push(true);
+
+console.log('union_types: ', _union_types);
