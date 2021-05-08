3d67ff75b5952f117283fc3a723431c241d4be27
mmkvdev <madhuv299@gmail.com>
2021-05-08 21:20:20 +0530

    ts/type_aliases: type aliases

---
+++ project/type-aliases/type-aliases.js
@@ -0,0 +1,10 @@
+var userData = function (id, name) {
+    console.log(id + " has an name of " + name);
+};
+var userConfigMapping = function (user) {
+    console.log(user.name + " has an id of: " + user.id);
+};
+var userConfigMappingExperiment = function (user) {
+    console.log(user.name + " has an id of: " + user.id);
+};
+//# sourceMappingURL=type-aliases.js.map
