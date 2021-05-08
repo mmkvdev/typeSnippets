9c0579d721c8a4c97381211745dce3110ee11be6
mmkvdev <madhuv299@gmail.com>
2021-05-08 21:17:11 +0530

    ts/explicit_variables: explicit variable types

---
+++ project/explicit-types/variables/explicit-variables.js
@@ -0,0 +1,13 @@
+// explicit types
+var developer_;
+var age_;
+var skills_;
+// age = 'aws';
+age_ = 24;
+skills_ = [
+    {
+        name: 'aws', techstack: ['python', 'aws']
+    },
+    { stack: 'python' }
+];
+//# sourceMappingURL=explicit-variables.js.map
