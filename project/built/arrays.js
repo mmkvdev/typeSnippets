e50cabdcb5e8d6a11d03805c6d2582ac2c4b6066
mmkvdev <madhuv299@gmail.com>
2021-05-08 21:13:24 +0530

    ts/build: compiled files

---
+++ project/built/arrays.js
@@ -0,0 +1,15 @@
+// arrays
+var arr_ = ['typescript', 'javascript', 'python'];
+arr_.push('php'); // valid
+// arr.push(3); // invalid => typescript fails the compilation because the type of the latest entries should match the type inside the types of elements declared inside the array.
+// arr[0] = 3; //invalid => can't assign a mistype
+// arr = 'ruby'; // can't change the type of the array variable as well
+var numbers_ = [1, 2, 3, 4];
+numbers_.push(5); //valid
+// numbers.push('developer'); //invalid
+// numbers[1] = 'software'; // invalid
+var multumArray = [1, 2, 'cloud', 'aws'];
+multumArray.push('developer');
+multumArray.push(3);
+multumArray[1] = 'devops';
+//# sourceMappingURL=arrays.js.map
