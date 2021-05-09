1d9eb9f9d4e3d606553aef8a695090e28d889a5a
mmkvdev <madhuv299@gmail.com>
2021-05-09 18:12:37 +0530

    ts/tuples: type script tuples

---
+++ project/public/tuples/tuples.js
@@ -0,0 +1,15 @@
+// tuples
+let arr = ['aws', 25, true];
+arr[0] = 25;
+arr[1] = 'dev';
+arr[2] = false;
+arr = [23, 'dev', 21];
+let tup = ['aws', 25, true];
+tup[0] = 'cloud';
+tup[1] = 31;
+tup[2] = false;
+let student;
+student[0] = 'mmk';
+student[1] = 1;
+student = ['dev', 2];
+//# sourceMappingURL=tuples.js.map
