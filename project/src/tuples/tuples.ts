b9ae50b7546ccb5ccae1ccfcd783112f44eaf9a6
mmkvdev <madhuv299@gmail.com>
2021-05-09 18:17:06 +0530

    ts/tuples: type script tuples

---
+++ project/src/tuples/tuples.ts
@@ -0,0 +1,20 @@
+// tuples
+
+let arr = ['aws', 25, true];
+arr[0] = 25;
+arr[1] = 'dev';
+arr[2] = false;
+
+arr = [23, 'dev', 21];
+
+
+let tup: [string, number, boolean] = ['aws', 25, true];
+tup[0] = 'cloud';
+tup[1] = 31;
+tup[2] = false;
+
+let student: [string, number];
+
+student[0] = 'mmk';
+student[1] = 1;
+student = ['dev', 2];
